0cc1decb08f358e3a6223fabfc3127d763d85635
RickFlinn <mriodine@gmail.com>
2019-01-16 15:03:40 -0800

    added basic frame for Deck custom collection and Card class. Added Add method for Deck.

---
+++ LabSeven_Collections/LabSeven_Collections/Classes/Card.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LabSeven_Collections.Classes;
+
+namespace LabSeven_Collections.Classes
+{
+    class Card
+    {
+
+        private int rank;
+
+
+        public int Rank
+        {
+            get => rank;
+            set
+            {
+                if (value >= 1 && value <= 13)
+                {
+                    rank = value;
+                } else
+                {
+                    rank = 0;
+                }
+            }
+        }
+
+        public Suit Suit { get; set; }
+
+
+        public Card (Suit givenSuit, int givenRank)
+        {
+            Suit = givenSuit;
+            Rank = givenRank;
+        }
+    }
+}
---
+++ LabSeven_Collections/LabSeven_Collections/Classes/Deck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabSeven_Collections.Classes
+{
+    enum Suit { Diamonds, Clubs, Spades, Hearts }
+
+
+    class Deck<T> : IEnumerable<T>
+    {
+        private T[] decko = new T[3];
+        private int nextIndex = 0;
+        
+        public void Add(T thing)
+        {
+            if (nextIndex == decko.Length)
+            {
+                Array.Resize<T>(ref decko, decko.Length*2);
+            }
+
+            decko[nextIndex] = thing;
+            nextIndex++;
+        }
+
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach(T thingy in decko)
+            {
+                yield return thingy;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+
+    }
+
+
+}
